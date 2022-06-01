b9c78c9b2c4e0ad33b4fdef0bd4f662cb5dece06
ryannguyen0303 <ryannguyen0303@gmail.com>
2022-06-01 22:25:46 +0700

    chore: add static metatag for homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,38 @@ const Home: NextPage = () => {
         <title>eCert - Electronic Certification</title>
         <meta name="description" content="Electronic Certification" />
         <link rel="icon" href="/favicon2.ico" />
+
+        <meta
+          name="description"
+          content="Verifying with eCert make things easier and safer"
+        />
+        <meta name="keywords" content="" />
+
+        <meta name="author" content="eCert" />
+        <meta name="copyright" content="eCert" />
+        <meta
+          name="application-name"
+          content="eCert - Electronic Certification"
+        />
+
+        <meta property="og:title" content="eCert - Electronic Certification" />
+        <meta property="og:type" content="article" />
+        <meta property="og:image" content="/home.png" />
+        <meta
+          property="og:description"
+          content="Verifying with eCert make things easier and safer"
+        />
+
+        <meta
+          name="twitter:card"
+          content="Verifying with eCert make things easier and safer"
+        />
+        <meta name="twitter:title" content="eCert - Electronic Certification" />
+        <meta
+          name="twitter:description"
+          content="Verifying with eCert make things easier and safer"
+        />
+        <meta name="twitter:image" content="/home.png" />
       </Head>
 
       <Navigation />
