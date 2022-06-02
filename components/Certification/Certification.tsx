1b9fcf85e197fb14dffe486e73cc6c319abb88aa
ryannguyen0303 <ryannguyen0303@gmail.com>
2022-06-02 14:02:16 +0700

    chore: update ui in certi page

--- components/Certification/Certification.tsx
+++ components/Certification/Certification.tsx
@@ -34,7 +34,7 @@ export const Certification = () => {
         </div>
       ) : !serverErr ? (
         <div className="container px-4 mx-auto">
-          <div className="max-w-7xl mx-auto mb-8 text-center">
+          <div className="max-w-5xl mx-auto mb-8 text-center">
             <span className="inline-block text-xs py-1 px-3 text-blue-700 font-semibold bg-blue-100 rounded-xl">
               Certificate Verification
             </span>
@@ -45,8 +45,31 @@ export const Certification = () => {
               Email: {uploadCert?.email}
             </p>
             <p className="text-blueGray-700 text-left pb-5 max-w-5xl mx-auto">
+              <h1 className="-ml-4 font-semibold mb-2">Description:</h1>
               {uploadCert?.data}
             </p>
+            <hr className="mt-10" />
+            <div className="pl-10 mb-5 grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+              <p className="text-blueGray-700 text-sm flex">
+                <p className="font-semibold">Created at: </p>
+                <p className="pl-2">
+                  {new Date(Date.parse(uploadCert?.createdAt)).toDateString()}
+                </p>
+              </p>
+              <p className="text-blueGray-700 text-sm flex">
+                <p className="font-semibold">Reference No: </p>
+                <p className="pl-2">{router.query.id}</p>
+              </p>
+              <p className="text-blueGray-700 text-sm flex">
+                <p className="font-semibold">Updated at: </p>
+                <p className="pl-2">{uploadCert?.updatedAt}</p>
+              </p>
+              <p className="text-blueGray-700 text-sm flex">
+                <p className="font-semibold">Organization id: </p>
+                <p className="pl-2">{uploadCert?.userId}</p>
+              </p>
+            </div>
+            <hr className="mb-10" />
           </div>
           <div className="relative max-w-6xl mx-auto text-center">
             <img
@@ -54,27 +77,6 @@ export const Certification = () => {
               src={uploadCert?.qrCode}
               alt=""
             />
-          </div>
-          <hr className="mt-10" />
-          <div className="pl-10 mb-20 grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
-            <p className="text-blueGray-700 text-sm flex">
-              <p className="font-semibold">Created at: </p>
-              <p className="pl-2">
-                {new Date(Date.parse(uploadCert?.createdAt)).toDateString()}
-              </p>
-            </p>
-            <p className="text-blueGray-700 text-sm flex">
-              <p className="font-semibold">Reference No: </p>
-              <p className="pl-2">{router.query.id}</p>
-            </p>
-            <p className="text-blueGray-700 text-sm flex">
-              <p className="font-semibold">Updated at: </p>
-              <p className="pl-2">{uploadCert?.updatedAt}</p>
-            </p>
-            <p className="text-blueGray-700 text-sm flex">
-              <p className="font-semibold">Organization id: </p>
-              <p className="pl-2">{uploadCert?.userId}</p>
-            </p>
           </div>
         </div>
       ) : (
